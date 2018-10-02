19041a1d129f587503f10de41db727e55f1da0c0
Amol Moses Jha <amolmoses@gmail.com>
2018-10-02 13:50:34 -0400

    Minor font changes

--- emacs/init.el
+++ emacs/init.el
@@ -48,5 +48,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#32302f" :foreground "#fdf4c1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "CTDB" :family "Fira Code")))))
+ '(default ((t (:inherit nil :stipple nil :background "#32302f" :foreground "#fdf4c1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 154 :width normal :foundry "CTDB" :family "Input Mono Medium")))))
 
